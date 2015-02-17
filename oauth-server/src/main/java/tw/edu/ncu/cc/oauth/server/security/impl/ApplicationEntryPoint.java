661549c27784a5dbcef2499355dc7cfb569be01a
jasonChiu <jason1122g@gmail.com>
2015-02-17 14:54:58 +0800

    rename entry point

--- oauth-server/src/main/java/tw/edu/ncu/cc/oauth/server/security/impl/ApplicationEntryPoint.java
+++ oauth-server/src/main/java/tw/edu/ncu/cc/oauth/server/security/impl/ApplicationEntryPoint.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class StubAuthenticationEntry implements AuthenticationEntryPoint {
+public class ApplicationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence( HttpServletRequest request, HttpServletResponse response, AuthenticationException authException ) throws IOException, ServletException {
