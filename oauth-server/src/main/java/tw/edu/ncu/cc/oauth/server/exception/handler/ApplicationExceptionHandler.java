10de231a76eadb542179528de84478aacd271371
jasonChiu <jason1122g@gmail.com>
2015-02-21 21:31:05 +0800

    add method not support exception handle

--- oauth-server/src/main/java/tw/edu/ncu/cc/oauth/server/exception/handler/ApplicationExceptionHandler.java
+++ oauth-server/src/main/java/tw/edu/ncu/cc/oauth/server/exception/handler/ApplicationExceptionHandler.java
@@ -6,11 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import tw.edu.ncu.cc.oauth.data.v1.message.ErrorCode;
 
 import javax.persistence.NoResultException;
+import java.util.Arrays;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
@@ -31,6 +33,15 @@ public class ApplicationExceptionHandler {
         return new ResponseEntity<>(
                 new tw.edu.ncu.cc.oauth.data.v1.message.Error(
                         ErrorCode.INVALID_BODY, "expect request in json format"
+                ), HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler( HttpRequestMethodNotSupportedException.class )
+    public ResponseEntity invalidMethod( HttpRequestMethodNotSupportedException e ) {
+        return new ResponseEntity<>(
+                new tw.edu.ncu.cc.oauth.data.v1.message.Error(
+                        ErrorCode.INVALID_METHOD, "method not supported:" + e.getMethod() + ", expect:" + Arrays.toString( e.getSupportedMethods() )
                 ), HttpStatus.BAD_REQUEST
         );
     }
