8d4e37cf242dd136a8e65f21c07c910f6b496106
jasonChiu <jason1122g@gmail.com>
2015-02-21 21:30:36 +0800

    add more error codes

--- oauth-data/src/main/java/tw/edu/ncu/cc/oauth/data/v1/message/ErrorCode.java
+++ oauth-data/src/main/java/tw/edu/ncu/cc/oauth/data/v1/message/ErrorCode.java
@@ -5,6 +5,8 @@ public class ErrorCode {
     public static final int NOT_EXIST = 0;
     public static final int INVALID_FIELD = NOT_EXIST + 1;
     public static final int INVALID_BODY = INVALID_FIELD + 1;
-    public static final int SERVER_ERROR = INVALID_BODY + 1;
+    public static final int INVALID_METHOD = INVALID_BODY + 1;
+    public static final int INVALID_HEADER = INVALID_METHOD + 1;
+    public static final int SERVER_ERROR = INVALID_HEADER + 1;
 
 }
