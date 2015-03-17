52d66360d8e04d43ac6f79a1b7a2e5e52d1612ee
jasonChiu <jason1122g@gmail.com>
2015-03-17 20:33:19 +0800

    remove redundant exception handler

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/exception/handler/ApplicationExceptionHandler.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/exception/handler/ApplicationExceptionHandler.groovy
@@ -2,7 +2,6 @@ package tw.edu.ncu.cc.oauth.server.exception.handler
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
@@ -12,21 +11,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import tw.edu.ncu.cc.oauth.data.v1.message.ErrorCode
 
-import javax.persistence.NoResultException
-
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
     private Logger logger = LoggerFactory.getLogger( this.getClass() );
-
-    @ExceptionHandler( [ NoResultException.class, EmptyResultDataAccessException.class ] )
-    public ResponseEntity resourseNotFound() {
-        return new ResponseEntity<>(
-                new tw.edu.ncu.cc.oauth.data.v1.message.Error(
-                        ErrorCode.NOT_EXIST, "resourse not exist"
-                ), HttpStatus.NOT_FOUND
-        );
-    }
 
     @ExceptionHandler( [ HttpMessageNotReadableException.class, HttpMediaTypeNotSupportedException.class ] )
     public ResponseEntity invalidRequestBody() {
