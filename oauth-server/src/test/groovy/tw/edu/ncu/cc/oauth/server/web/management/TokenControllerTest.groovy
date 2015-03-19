4386db22c8da1987bbface8489080dc51c634f2a
jasonChiu <jason1122g@gmail.com>
2015-03-19 18:12:49 +0800

    add more token controller test

--- oauth-server/src/test/groovy/tw/edu/ncu/cc/oauth/server/web/management/TokenControllerTest.groovy
+++ oauth-server/src/test/groovy/tw/edu/ncu/cc/oauth/server/web/management/TokenControllerTest.groovy
@@ -1,7 +1,9 @@
 package tw.edu.ncu.cc.oauth.server.web.management
 
+import org.springframework.transaction.annotation.Transactional
 import specification.IntegrationSpecification
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -9,7 +11,7 @@ class TokenControllerTest extends IntegrationSpecification {
 
     def targetURL = "/management/v1/token"
 
-    def "it can handle get of specified AccessToken"() {
+    def "user can get access token info by id"() {
         when:
             def response = JSON(
                     server().perform(
@@ -20,6 +22,35 @@ class TokenControllerTest extends IntegrationSpecification {
             )
         then:
             response.user == "ADMIN1"
+    }
+
+    def "user can get access token info by token"() {
+        when:
+            def response = JSON(
+                    server().perform(
+                            get( targetURL + "/string/Mzo6OlRPS0VO" )
+                    ).andExpect(
+                            status().isOk()
+                    ).andReturn()
+            )
+        then:
+            response.user == "ADMIN3"
+    }
+
+    @Transactional
+    def "user can delete access token by id"() {
+        when:
+            server().perform(
+                    delete( targetURL + "/1" )
+            ).andExpect(
+                    status().isOk()
+            )
+        then:
+            server().perform(
+                    get( targetURL + "/1" )
+            ).andExpect(
+                    status().isNotFound()
+            )
     }
 
 }
