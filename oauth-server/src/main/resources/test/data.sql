a06ca8e8f1872dd8a2dd1a8292845aea6f6e745d
jasonChiu <jason1122g@gmail.com>
2015-03-22 16:57:01 +0800

    add test data for refresh token

--- oauth-server/src/main/resources/test/data.sql
+++ oauth-server/src/main/resources/test/data.sql
@@ -7,6 +7,19 @@ INSERT INTO client ( id, name, secret, url, callback, description, owner_id ) VA
   ( 1, 'APP1', 'SECRET1', 'http://example.com', 'http://example.com', '1111', 1 ),
   ( 2, 'APP2', 'SECRET2', 'http://example.com', 'http://example.com', '2222', 2 ),
   ( 3, 'APP3', '$2a$10$Sm07H4pIys2Ae45rdO5rJuiEe/swFKoJneZLg.bU9HjFsBBZktwua', 'http://example.com', 'http://example.com', '3333', 2 );
+-- SECRET
+
+INSERT INTO refresh_token ( id, token, access_token_id, client_id, user_id, date_created, last_updated, date_expired ) VALUES
+  ( 1, 'TOKEN1', 1, 1, 1, '2050-12-25', '2050-12-25', '2100-12-25' ),
+  ( 2, 'TOKEN2', 2, 2, 2, '2050-12-25', '2050-12-25', '2000-12-25' ),
+  ( 3, '$2a$10$dEFh.Tw.s05K66oj0gOopObZePAyYRGuTCkBLN5dvrdfS4x2RxtHO', 3, 3, 3, '2050-12-25', '2050-12-25', '2100-12-25' );
+-- Mzo6OlRPS0VO
+
+INSERT INTO refresh_token_scope ( permission_id, refresh_token_id ) VALUES
+  ( 1, 1 ),
+  ( 2, 1 ),
+  ( 1, 3 ),
+  ( 2, 3 );
 
 INSERT INTO access_token ( id, token, client_id, user_id, date_created, last_updated, date_expired ) VALUES
   ( 1, 'TOKEN1', 1,  1, '2050-12-25', '2050-12-25', '2100-12-25' ),
