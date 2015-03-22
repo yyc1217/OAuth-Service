acb5cc81816856f555baffac6fe270addcbf76a6
jasonChiu <jason1122g@gmail.com>
2015-03-22 17:14:30 +0800

    update tests

--- oauth-server/src/test/groovy/tw/edu/ncu/cc/oauth/server/domain/ClientTest.groovy
+++ oauth-server/src/test/groovy/tw/edu/ncu/cc/oauth/server/domain/ClientTest.groovy
@@ -1,32 +1,18 @@
 package tw.edu.ncu.cc.oauth.server.domain
 
-import org.springframework.transaction.annotation.Transactional
 import specification.SpringSpecification
-
 
 class ClientTest extends SpringSpecification {
 
-    @Transactional
     def "it can map to exist data"() {
         given:
-            def client = Client.get( 1 )
+            def client = Client.include( [ 'owner' ] ).get( 1 )
         expect:
             client.name == 'APP1'
             client.url  == 'http://example.com'
             client.callback == 'http://example.com'
             client.description == '1111'
             client.owner.name == 'ADMIN1'
-    }
-
-    @Transactional
-    def "it's description and url can be null"() {
-        given:
-            def client = Client.get( 1 )
-        when:
-            client.url = null
-            client.description = null
-        then:
-            client.save() != null
     }
 
 }
