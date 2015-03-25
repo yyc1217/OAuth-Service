784fb0f48b8198b0bc811bdc947b8e805d961cbd
jasonChiu <jason1122g@gmail.com>
2015-03-25 20:08:29 +0800

    let api token be updated when read

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/service/domain/ApiTokenServiceImpl.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/service/domain/ApiTokenServiceImpl.groovy
@@ -20,6 +20,7 @@ class ApiTokenServiceImpl implements ApiTokenService {
         Client client = Client.findWhere( id: serialSecret.id )
         if( client != null && secretService.matchesSecret( serialSecret.secret, client.apiToken ) ) {
             client.apiUseTimes += 1
+            client.updateTimeStamp()
             client.save( failOnError: true )
         } else {
             return null
