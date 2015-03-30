e8cc0038434fa9a0f5843788147a07344ef92f2e
jasonChiu <jason1122g@gmail.com>
2015-03-30 17:08:03 +0800

    migrate remote config

---
+++ oauth-resource/src/main/groovy/tw/edu/ncu/cc/oauth/resource/config/RemoteConfig.groovy
@@ -0,0 +1,9 @@
+package tw.edu.ncu.cc.oauth.resource.config;
+
+public class RemoteConfig {
+
+    def String serverPath = "https://localhost/oauth";
+    def static final String accessTokenPath = "/management/v1/access_token";
+    def static final String apiTokenPath    = "/management/v1/api_token";
+
+}
