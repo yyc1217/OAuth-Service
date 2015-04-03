d083e5c33f5a75ab723337f12f46fb4cbce56d8a
jasonChiu <jason1122g@gmail.com>
2015-04-03 17:02:33 +0800

    update api token service

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/service/domain/ApiTokenService.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/service/domain/ApiTokenService.groovy
@@ -1,9 +1,10 @@
 package tw.edu.ncu.cc.oauth.server.service.domain
 
-import tw.edu.ncu.cc.oauth.server.domain.Client
-
+import tw.edu.ncu.cc.oauth.server.domain.ApiToken
 
 interface ApiTokenService {
 
-    Client readAndUseByRealToken( String token );
+    ApiToken create( ApiToken apiToken )
+    ApiToken readAndUseByRealToken( String token )
+    ApiToken readAndUseByRealToken( String token, List includeField )
 }
