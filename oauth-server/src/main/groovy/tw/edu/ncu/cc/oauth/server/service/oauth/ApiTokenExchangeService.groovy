e1fcaa71a4d0a7057c0a45b623730310a3f12da2
jasonChiu <jason1122g@gmail.com>
2015-04-03 17:05:41 +0800

    add api token exchange service

---
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/service/oauth/ApiTokenExchangeService.groovy
@@ -0,0 +1,89 @@
+package tw.edu.ncu.cc.oauth.server.service.oauth
+
+import org.apache.oltu.oauth2.as.request.OAuthTokenRequest
+import org.apache.oltu.oauth2.common.error.OAuthError
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import tw.edu.ncu.cc.oauth.server.domain.ApiToken
+import tw.edu.ncu.cc.oauth.server.helper.StringHelper
+import tw.edu.ncu.cc.oauth.server.helper.TimeBuilder
+import tw.edu.ncu.cc.oauth.server.helper.data.TimeUnit
+import tw.edu.ncu.cc.oauth.server.service.domain.ApiTokenService
+import tw.edu.ncu.cc.oauth.server.service.domain.ClientService
+
+import javax.servlet.http.HttpServletResponse
+
+@Service( "ApiTokenExchangeService" )
+class ApiTokenExchangeService implements TokenExchangeService {
+
+    @Autowired
+    def ClientService clientService
+
+    @Autowired
+    def ApiTokenService apiTokenService
+
+    private Logger logger = LoggerFactory.getLogger( this.getClass() )
+
+    @Override
+    @Transactional
+    String buildResonseMessage( OAuthTokenRequest request, long expireSeconds ) throws OAuthProblemException, OAuthSystemException {
+
+        validateOauthRequest( request )
+
+        ApiToken apiToken = prepareApiToken( request, expireSeconds )
+
+        return buildResponseMessage( apiToken.token, expireSeconds )
+    }
+
+    private void validateOauthRequest( OAuthTokenRequest request ) {
+
+        String clientID     = request.getClientId()
+        String clientSecret = request.getClientSecret()
+
+        logger.info(
+                String.format(
+                        "OAUTH EXCHANGE APITOKEN, CLIENT: %s",
+                        StringHelper.first( clientID, 10 )
+                )
+        )
+
+        if ( ! clientService.isCredentialValid( clientID, clientSecret ) ) {
+            throw OAuthProblemException.error(
+                    OAuthError.TokenResponse.INVALID_CLIENT, "INVALID CLIENT"
+            )
+        }
+    }
+
+    private ApiToken prepareApiToken( OAuthTokenRequest request, long expireSeconds ) {
+        apiTokenService.create(
+                new ApiToken(
+                        client: clientService.readBySerialId( request.getClientId() ),
+                        dateExpired: dicideExpireDate( expireSeconds )
+                )
+        )
+    }
+
+    private static Date dicideExpireDate( long expireSeconds ) {
+        if( expireSeconds <= 0 ) {
+            return TimeBuilder.now().after( 30, TimeUnit.DAY ).buildDate()
+        } else {
+            return TimeBuilder.now().after( expireSeconds, TimeUnit.SECOND ).buildDate()
+        }
+    }
+
+    private static String buildResponseMessage( String apiToken, long expireSeconds ) {
+        return org.apache.oltu.oauth2.as.response.OAuthASResponse
+                .tokenResponse( HttpServletResponse.SC_OK )
+                .setAccessToken( apiToken )
+                .setTokenType( "Bearer" )
+                .setExpiresIn( expireSeconds as String )
+                .buildJSONMessage()
+                .getBody()
+    }
+
+}
