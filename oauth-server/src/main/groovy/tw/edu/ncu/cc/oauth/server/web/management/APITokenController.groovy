e39fc34221e1277ab8fbe378a27404849b723dcc
jasonChiu <jason1122g@gmail.com>
2015-04-06 11:58:19 +0800

    update api token request using header

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/web/management/APITokenController.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/web/management/APITokenController.groovy
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.convert.ConversionService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -28,8 +28,8 @@ class APITokenController {
     @Value( '${custom.api.limit-times}' )
     def long api_limit_times
 
-    @RequestMapping( value = "{token}", method = RequestMethod.GET )
-    public ResponseEntity getToken( @PathVariable( "token" ) final String token ) {
+    @RequestMapping( method = RequestMethod.GET )
+    public ResponseEntity getTokenByToken( @RequestHeader( "token" ) final String token ) {
         respondWith(
                 resource()
                 .pipe {
