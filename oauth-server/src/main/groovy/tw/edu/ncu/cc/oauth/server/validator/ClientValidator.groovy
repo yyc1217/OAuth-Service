8e8adb9743f2c5d5ed1d0891c9de944df834f60c
jasonChiu <jason1122g@gmail.com>
2015-04-08 11:50:05 +0800

    update client validator

---
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/validator/ClientValidator.groovy
@@ -0,0 +1,29 @@
+package tw.edu.ncu.cc.oauth.server.validator
+
+import org.springframework.util.StringUtils
+import org.springframework.validation.Errors
+import org.springframework.validation.Validator
+import tw.edu.ncu.cc.oauth.data.v1.management.client.ClientObject
+
+public class ClientValidator implements Validator {
+
+    @Override
+    public boolean supports( Class< ? > clazz ) {
+        return ClientObject.class.equals( clazz )
+    }
+
+    @Override
+    public void validate( Object target, Errors errors ) {
+        ClientObject clientObject = ( ClientObject ) target
+        if( StringUtils.isEmpty( clientObject.getName() ) ) {
+            errors.rejectValue( "name", "name.necessary", "name is necessary" )
+        }
+        if( StringUtils.isEmpty( clientObject.getCallback() ) ) {
+            errors.rejectValue( "callback", "callback.necessary", "callback is necessary" )
+        }
+        if(  StringUtils.isEmpty( clientObject.getOwner() ) ) {
+            errors.rejectValue( "owner", "owner.necessary", "owner is necessary" )
+        }
+    }
+
+}
