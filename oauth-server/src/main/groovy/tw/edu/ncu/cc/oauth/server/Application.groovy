bc790cd040e9fc77493a4444b1f604d655377880
jasonChiu <jason1122g@gmail.com>
2015-04-09 18:03:41 +0800

    enable scheduling

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/Application.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/Application.groovy
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.web.SpringBootServletInitializer
 import org.springframework.context.annotation.Import
+import org.springframework.scheduling.annotation.EnableScheduling
 import tw.edu.ncu.cc.oauth.server.config.BeanConfig
 import tw.edu.ncu.cc.oauth.server.config.CacheConfig
 import tw.edu.ncu.cc.oauth.server.config.MvcConfig
 import tw.edu.ncu.cc.oauth.server.config.SecurityConfig
 
+@EnableScheduling
 @SpringBootApplication( exclude = [ ErrorMvcAutoConfiguration ] )
 @Import( [ SecurityConfig, BeanConfig, CacheConfig, MvcConfig ] )
 public class Application extends SpringBootServletInitializer {
