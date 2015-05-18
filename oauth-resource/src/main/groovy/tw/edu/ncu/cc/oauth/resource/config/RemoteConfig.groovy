8c8a5784093d780b2a08515b6a4c503cdc0121b5
jasonChiu <jason1122g@gmail.com>
2015-05-18 21:47:06 +0800

    update resource remote config

--- oauth-resource/src/main/groovy/tw/edu/ncu/cc/oauth/resource/config/RemoteConfig.groovy
+++ oauth-resource/src/main/groovy/tw/edu/ncu/cc/oauth/resource/config/RemoteConfig.groovy
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class RemoteConfig {
 
     def String serverPath = "https://localhost/oauth";
-    def static final String accessTokenPath = "/management/v1/access_tokens";
-    def static final String apiTokenPath    = "/management/v1/api_tokens";
+    def static final String accessTokenPath = "/management/v1/access_tokens/token";
+    def static final String apiTokenPath    = "/management/v1/api_tokens/token";
 
 }
