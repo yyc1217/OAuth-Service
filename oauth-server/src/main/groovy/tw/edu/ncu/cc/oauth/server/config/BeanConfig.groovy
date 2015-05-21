7dd54caafc4d9e12c0eacd3bb19f65f1fa76c1ac
jasonChiu <jason1122g@gmail.com>
2015-05-21 19:30:16 +0800

    remove unused bean

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/config/BeanConfig.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/config/BeanConfig.groovy
@@ -4,17 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import tw.edu.ncu.cc.manage.openid.OpenIDManager
 
 @Configuration
 class BeanConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     @Autowired
