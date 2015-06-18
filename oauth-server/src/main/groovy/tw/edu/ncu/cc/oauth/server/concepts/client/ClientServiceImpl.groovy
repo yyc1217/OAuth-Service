ac942e1c7d8a1adb361730f16711390bb07a586d
Yeh-Yung <yyc1217@gmail.com>
2015-06-18 16:06:40 +0800

    add ClientServiceImpl.groovy

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/concepts/client/ClientServiceImpl.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/concepts/client/ClientServiceImpl.groovy
@@ -2,9 +2,17 @@ package tw.edu.ncu.cc.oauth.server.concepts.client
 
 import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
+import org.springframework.util.StringUtils
+import tw.edu.ncu.cc.oauth.data.v1.management.client.IdClientObject
 import tw.edu.ncu.cc.oauth.server.concepts.security.SecretService
+import tw.edu.ncu.cc.oauth.server.concepts.user.UserService
 
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.CriteriaQuery
+import javax.persistence.criteria.Predicate
+import javax.persistence.criteria.Root
 import javax.persistence.metamodel.Attribute
 
 import static org.springframework.data.jpa.domain.Specifications.where
@@ -12,6 +20,9 @@ import static org.springframework.data.jpa.domain.Specifications.where
 @Service
 @CompileStatic
 class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    def UserService userService
 
     @Autowired
     def SecretService secretService
@@ -64,4 +75,33 @@ class ClientServiceImpl implements ClientService {
         }
     }
 
+    @Override
+    List<Client> findByDTO(IdClientObject dto)  {
+        clientRepository.findAll(new Specification<Client>() {
+            @Override
+            public Predicate toPredicate(Root<Client> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+
+                def predicates = []
+
+                if (!StringUtils.isEmpty(dto.id)) {
+                    predicates.add(cb.equal(root.get("id"), dto.id as Integer))
+                }
+
+                if (!StringUtils.isEmpty(dto.name)) {
+                    predicates.add(cb.like(root.get(Client_.name), "%${dto.name}%"))
+                }
+
+                if (!StringUtils.isEmpty(dto.owner)) {
+                    def user = userService.findByName(dto.owner)
+                    if (user != null) {
+                        predicates.add(cb.equal(root.get(Client_.owner), user))
+                    }
+                }
+
+                predicates.add(cb.equal(root.get(Client_.deleted), dto.isDeleted))
+
+                cb.and(predicates as Predicate[])
+            }
+        })
+    }
 }
